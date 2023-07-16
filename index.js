e57e50eb0c72d2c16d8f277c5e7353503d454e88
flippy873 <98759821+flippy873@users.noreply.github.com>
2023-07-16 21:32:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const toggler = document.querySelector(".toggler");
 const navMenu = document.querySelector("#navMenu");
 
-toggler.addEventListener("click", function () {
+toggler.addEventListener('click', function () {
     navMenu.classList.toggle("active")
 });
 
 const scroll = document.getElementById("scroll");
 
-scroll.addEventListener("click", () => {
-    document.querySelector(".get-started").scrollIntoView({ behavior: 'smooth'});
+scroll.addEventListener('click', () => {
+    document.querySelector(".get-started").scrollIntoView({ behavior: 'smooth' });
 })
