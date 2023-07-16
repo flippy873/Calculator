5cd201d28c8ebdd78e0ec0878d4b89052cd2d5f1
flippy873 <98759821+flippy873@users.noreply.github.com>
2023-07-16 18:18:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-let ratioWidth = document.getElementById("ratio-width")
-let ratioHeight = document.getElementById("ratio-height")
-let width = document.getElementById("width")
-let height = document.getElementById("height")
+const toggler = document.querySelector(".toggler");
+const navMenu = document.querySelector("#navMenu");
 
-let calculateWidth = () => {
-    let aspectRatio = ratioWidth.value / ratioHeight.value;
-    width.value = parseFloat((height.value * aspectRatio).toFixed(2));
-};
+toggler.addEventListener("click", function () {
+    navMenu.classList.toggle("active")
+});
 
-let calculateHeight = () => {
-    let aspectRatio = ratioWidth.value / ratioHeight.value;
-    height.value = parseFloat((width.value * aspectRatio).toFixed(2));
-};
+const scroll = document.getElementById("scroll");
 
-height.addEventListener("input", calculateWidth);
-width.addEventListener("input", calculateHeight);
-ratioHeight.addEventListener("input", calculateWidth);
-ratioWidth.addEventListener("input", calculateHeight);
+scroll.addEventListener("click", () => {
+    document.querySelector(".get-started").scrollIntoView({ behavior: 'smooth'});
+})
